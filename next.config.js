f0f2f146644ac5adbc930c4cd938ab42425dfe2b
Luis Silva <l.silva06@ufromail.cl>
2024-03-18 16:52:11 -0300

    config: update next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
-// @ts-check
-
-/** @type {import('next').NextConfig} */
-
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
   basePath: '/briefcase',
-};
-
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
 module.exports = nextConfig
