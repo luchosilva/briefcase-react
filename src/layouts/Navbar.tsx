65a10ff64af45837f21dc299d414772b8cc75259
Luis Silva <l.silva06@ufromail.cl>
2024-03-25 09:17:09 -0300

    feat: improvements in navbar

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -10,7 +10,7 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ open, setOpen }: NavbarProps) => {
-  const { i18n } = useTranslation('navbar');
+  const { t, i18n } = useTranslation('navbar');
 
   const availableLocales = i18n.options.supportedLngs || [];
 
@@ -22,7 +22,10 @@ export const Navbar = ({ open, setOpen }: NavbarProps) => {
 
   return (
     <nav className="flex justify-between items-center h-20 py-4 bg-blue-600-color px-7">
-      {theme === 'dark' ? <MoonIcon className="w-12 min-w-[40px] rounded-md hover:text-blue-color" /> : <SunIcon className="w-12 min-w-[40px] rounded-md hover:text-blue-color" />}
+      <div className="flex">
+        {theme === 'dark' ? <MoonIcon className="w-12 min-w-[40px] rounded-md hover:text-blue-color" /> : <SunIcon className="w-12 min-w-[40px] rounded-md hover:text-blue-color" />}
+        {i18n.language === 'es' ? <div className="w-12 min-w-[40px] rounded-md hover:text-blue-color">🇪🇸</div> : <div className="w-12 min-w-[40px] rounded-md hover:text-blue-color">🇺🇸</div>}
+      </div>
       {/* <Bars4Icon onClick={() => setOpen(!open)} className="w-12 min-w-[40px] rounded-md hover:text-blue-color cursor-pointer" /> */}
       <div className="flex items-center gap-3">
         {/* Language Selector */}
@@ -32,6 +35,7 @@ export const Navbar = ({ open, setOpen }: NavbarProps) => {
           </button>
           <ul className="menu menu-compact dropdown-content z-[1] mt-3 p-2 shadow bg-gray-50 bg-opacity-10 rounded-lg w-40 border dark:border-none">
             {availableLocales.map((availableLocale: string) => {
+              if (availableLocale === i18n.language || availableLocale === 'cimode') return null;
               return (
                 <li key={availableLocale}>
                   <button
@@ -41,7 +45,7 @@ export const Navbar = ({ open, setOpen }: NavbarProps) => {
                     onKeyDown={() => changeLocale(availableLocale)}
                     className="cursor-pointer"
                   >
-                    {availableLocale}
+                    {availableLocale === 'es' ? 'Spanish' : 'Inglés'} {availableLocale === 'es' ? '🇪🇸' : '🇺🇸'}
                   </button>
                 </li>
               );
@@ -58,15 +62,13 @@ export const Navbar = ({ open, setOpen }: NavbarProps) => {
             <li>
               <button onClick={() => changeTheme('light')} className="cursor-pointer" onKeyDown={() => changeTheme('light')}>
                 <SunIcon className="w-5 h-5" />
-                {/* $t('landing.nav.themeSelector.light') */}
-                Light
+                {t('themeSelector.light')}
               </button>
             </li>
             <li>
               <button onClick={() => changeTheme('dark')} className="cursor-pointer" onKeyDown={() => changeTheme('dark')}>
                 <MoonIcon className="w-5 h-5" />
-                {/*$t('landing.nav.themeSelector.dark')*/}
-                Dark
+                {t('themeSelector.dark')}
               </button>
             </li>
           </ul>
